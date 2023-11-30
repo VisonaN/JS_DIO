7901e8493801f0deba99ec234d8b4afe3ec705c3
VisonaN <paulovisona@gmail.com>
2023-11-30 17:04:23 -0300

    js_dio

---
+++ aulas/aula.js
@@ -0,0 +1,14 @@
+/* No JS temos duas formas de declarar variáveis let e var
+let variavel = 10; // Pode alterar o valor mesmo depois de declarada
+
+console.log(variavel);
+
+variavel =20;
+const pi = 3.14; // Não pode alterar o valor depois de ser declarada
+
+console.log(variavel);
+*/
+
+let variavel = 10;
+
+console.log(variavel);
