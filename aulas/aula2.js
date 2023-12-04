baaf04e78e0a9884fc46f8003b5f5ae7882b1a4f
VisonaN <paulovisona@gmail.com>
2023-12-04 09:33:13 -0300

    estruturas condicionais

--- aulas/aula2.js
+++ aulas/aula2.js
@@ -1,14 +1,27 @@
 // Estruturas condicionais
 
-const numero = 11;
+const numero = 0;
 
 const eNumeroPar = (numero % 2) === 0;
-console.log(eNumeroPar);
+//console.log(eNumeroPar);
 
 if (eNumeroPar) {
     console.log('Par')
-};
+} else {
+    console.log('Ímpar');
+}
 
-if (!eNumeroPar) {
-    console.log('Ímpar')
-};
+console.log('-----------------------------------------------------------------')
+
+const inumero = 15;
+
+const numeroDivisivelPor5 = (inumero % 5) === 0;
+//console.log(eNumeroPar);
+
+if (inumero === 0) {
+    console.log('O número é inválido');
+} else if (numeroDivisivelPor5) { // Pode ter quantos else if quiser
+    console.log('Sim');
+} else { // Não pode colocar um else no meio do nada sem que seja para finalizar
+    console.log('Não');
+}
