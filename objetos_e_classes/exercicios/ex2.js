229a42ece824b21ac3157590e415f3669f844dab
VisonaN <paulovisona@gmail.com>
2023-12-06 17:08:52 -0300

    conclui ex2

--- objetos_e_classes/exercicios/ex2.js
+++ objetos_e_classes/exercicios/ex2.js
@@ -5,3 +5,41 @@
  * Instancie uma pessoa chamada Paulo que tenha 58kg. 1.65 de altura e faça paulo falar o valor do seu IMC 
 */
 
+class Pessoa {
+    nome;
+    peso;
+    altura;
+
+    constructor(nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    falarImc() {
+        return this.peso / Math.pow(this.altura, 2);
+    }
+
+    classificarImc() {
+        const imc = this.falarImc();
+
+        if (imc < 18.5) {
+            return (`${imc.toFixed(2)}, ou seja, abaixo do peso.`);
+        } else if (imc >= 18.5 && imc < 25) {
+            return (`${imc.toFixed(2)}, ou seja, peso normal.`);
+        } else if (imc >= 25 && imc < 30) {
+            return (`${imc.toFixed(2)}, ou seja, acima do peso.`);
+        } else if (imc >= 30 && imc < 40) {
+            return (`${imc.toFixed(2)}, ou seja, obeso.`);
+        } else {
+            return (`${imc.toFixed(2)}, ou seja, obesidade grave.`);
+        };
+    }
+}
+
+const paulo = new Pessoa('Paulo Visoná Neto', 58, 1.65);
+console.log(`Meu nome é ${paulo.nome} e meu IMC é: ${paulo.classificarImc()}.`);
+const mylla = new Pessoa('Mylla Machado dos Reis', 48, 1.63);
+console.log(`Meu nome é ${mylla.nome} e meu IMC é: ${mylla.classificarImc()}.`);
+const jubis = new Pessoa('Jubileu Irineu', 6, 56);
+console.log(`Meu nome é ${jubis.nome} e meu IMC é: ${jubis.classificarImc()}.`);
