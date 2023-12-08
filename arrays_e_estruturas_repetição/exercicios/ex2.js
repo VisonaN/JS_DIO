8314e6b3697d3ef1ba275484f97419dd3b3d0a65
VisonaN <paulovisona@gmail.com>
2023-12-08 17:12:50 -0300

    mesmo exercício anterior so que mudei pra um gerador de numeros aleatórios

--- arrays_e_estruturas_repetição/exercicios/ex2.js
+++ arrays_e_estruturas_repetição/exercicios/ex2.js
@@ -1,17 +1,37 @@
 // Crie um programa que seja capaz de percorrer uma lista de numeros e imprima cada numero par encontrado
 
+let numeroAleatorio = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio2 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio3 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio4 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio5 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio6 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio7 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio8 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio9 = Math.floor(Math.random() * (100 - 10 + 1));
+
+let numeroAleatorio10 = Math.floor(Math.random() * (100 - 10 + 1));
+
 const numeros = [];
 
-numeros.push(1);
-numeros.push(2);
-numeros.push(3);
-numeros.push(4);
-numeros.push(5);
-numeros.push(6);
-numeros.push(7);
-numeros.push(8);
-numeros.push(9);
-numeros.push(10);
+numeros.push(numeroAleatorio);
+numeros.push(numeroAleatorio2);
+numeros.push(numeroAleatorio3);
+numeros.push(numeroAleatorio4);
+numeros.push(numeroAleatorio5);
+numeros.push(numeroAleatorio6);
+numeros.push(numeroAleatorio7);
+numeros.push(numeroAleatorio8);
+numeros.push(numeroAleatorio9);
+numeros.push(numeroAleatorio10);
 
 for (let i = 0; i < numeros.length; i++) {
     const numeroPar = numeros[i];
