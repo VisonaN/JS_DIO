48dca4e8a718b245d03425c5714992ec0274159c
VisonaN <paulovisona@gmail.com>
2023-12-11 09:09:02 -0300

    criei importação e exportação

---
+++ importacao/main.js
@@ -0,0 +1,18 @@
+const funcoes =  require('./func_auxiliares');
+
+/**
+ * Fazendo o object destrcturing com o objeto de cima
+    const { gets, print } =  require('./func_auxiliares');
+    print(gets());
+ */
+
+console.log(funcoes.gets());
+
+// Object destructuring
+
+// const pessoa = {
+//     nome: 'Paulo'
+// }
+
+// const { nome } = pessoa;
+// const nome = pessoa.nome;
