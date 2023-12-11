eddce8fa7f64e4ac76d7e60554c28978edd01a62
VisonaN <paulovisona@gmail.com>
2023-12-11 10:05:27 -0300

    numeros_sorteados

---
+++ importacao/exercicio/main.js
@@ -0,0 +1,19 @@
+const { gets, print } = require('./funcoes_auxiliares');
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numerosSorteados = gets();
+    numerosSorteados.push(numerosSorteados);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numerosSorteados = numerosSorteados[i];
+    if (numerosSorteados > maiorValor) {
+        maiorValor = numerosSorteados;
+    }
+}
+
+print(maiorValor)
