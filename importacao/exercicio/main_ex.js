5df0474e41732fa26f76f6a3c6caf10174cf4600
VisonaN <paulovisona@gmail.com>
2023-12-11 10:55:46 -0300

    nao atualiza o indice aaaa

--- importacao/exercicio/main_ex.js
+++ importacao/exercicio/main_ex.js
@@ -26,10 +26,10 @@ const quantidadeDeAlunos = gets();
 let maiorValorEncontrado = 0;
 
 for (let i = 0; i < quantidadeDeAlunos; i++) {
-    const numerosSorteado = gets();
-    if (numerosSorteado > maiorValorEncontrado) {
-        maiorValorEncontrado = numerosSorteado;
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
     }
 }
 
-print(maiorValorEncontrado);
+print(maiorValorEncontrado)
